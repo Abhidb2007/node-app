4985947669b0067c68629e67a4986ec469bda6d3
Abhidb2007 <abhishekdb2210@gmail.com>
2025-09-18 16:05:43 +0530

    code

--- src/a.ts
+++ src/a.ts
@@ -1,6 +1,9 @@
-type Input=number|string;
-function firstEl(arr:Input[]){
-    return arr[0];
+function identity<T>(arg: T): T {
+    return arg;
 }
-const value=firstEl(["ahi","asdf"])
-console.log(value);
+
+let output1 = identity<string>("myString");
+let output2 = identity<number>(100);
+
+console.log(output1.toUpperCase()); // Works, because output1 is string
+console.log(output2.toFixed(2));    // Works, because output2 is number
