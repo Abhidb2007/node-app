980b12a8346d7ce4f37f43302185e1195b61f723
Abhidb2007 <abhishekdb2210@gmail.com>
2025-09-20 14:56:18 +0530

    number code

--- src/a.ts
+++ src/a.ts
@@ -1,5 +1,13 @@
-const greet = (name: string): string => {
-  return `Hello, ${name}`;
-};
-
-console.log(greet("Abhishek"));
+function greet(name:string):string{
+    return "Hello" + name
+}
+function isEven(num: number):boolean{
+    if(num%2==0){
+        return true
+        
+    }else{
+        return false
+    }
+}
+const greeting=greet("123")
+console.log(greeting)
