53ab3519d68667d4e2ae705abb6060b0b6e98972
Abhidb2007 <abhishekdb2210@gmail.com>
2025-09-22 21:49:45 +0530

    code

--- src/a.js
+++ src/a.js
@@ -1,18 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Direction;
-(function (Direction) {
-    Direction[Direction["Up"] = 0] = "Up";
-    Direction[Direction["Down"] = 1] = "Down";
-    Direction[Direction["Right"] = 2] = "Right";
-    Direction[Direction["left"] = 3] = "left";
-})(Direction || (Direction = {}));
-function doSomething(keyPressed) {
-    if (keyPressed === Direction.Up) {
-    }
+function greet(user) {
+    console.log(user.name);
 }
-doSomething(Direction.Up);
-doSomething(Direction.Down);
-doSomething(Direction.Right);
-console.log(Direction.Up);
 //# sourceMappingURL=a.js.map
